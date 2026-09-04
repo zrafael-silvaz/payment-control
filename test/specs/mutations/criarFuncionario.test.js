986c97d945f73d94c5bb1d6c92958f6553e37c71
rafael <zrafael.silvaz@gmail.com>
2026-09-04 19:24:19 -0300

    add: mais testes

---
+++ test/specs/mutations/criarFuncionario.test.js
@@ -0,0 +1,126 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+describe.only('Criar funcionarios', () => {
+
+    let cpf = Date.now()
+    let nome = "teste funcionanrio"
+    let salario_base = 10000
+    let admissao = "2026-09-04"
+    let desligamento = ""
+
+    let token
+
+    before(async ()=>{
+        token = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: "admin@admin.com",
+                    senha: "123456"
+                }
+            })
+        expect(token.status).to.eq(200);
+        expect(token.body.data.login.token).not.be.empty;
+        expect(token.body.data.login.token).to.be.a.string;
+    })
+    it('deve criar um funcionario com os dados validos', async () => {
+
+        const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .set('Authorization', `Bearer ${token.body.data.login.token}`)
+            .send({
+                query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+  }
+}`,
+                variables: {
+                    "input": {
+                        cpf: cpf.toString(),
+                        nome: nome,
+                        salario_base: salario_base,
+                        admissao: admissao,
+                        desligamento: desligamento
+                    }
+                }
+            })
+        expect(response.status).to.eq(200);
+        expect(response.body.data.criarFuncionario.id).not.be.empty;
+        expect(response.body.data.criarFuncionario.id).to.be.a.string;
+        expect(response.body.data.criarFuncionario.cpf).to.be.eq(cpf.toString());
+        expect(response.body.data.criarFuncionario.nome).to.be.eq(nome);
+        expect(response.body.data.criarFuncionario.salario_base).to.be.eq(salario_base);
+        expect(response.body.data.criarFuncionario.admissao).to.be.eq(admissao);
+    })
+
+    it('nao deve criar um funcionario com o salario base vazio', async () => {
+
+        const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .set('Authorization', `Bearer ${token.body.data.login.token}`)
+            .send({
+                query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+  }
+}`,
+                variables: {
+                    "input": {
+                        cpf: cpf.toString(),
+                        nome: nome,
+                        salario_base: "",
+                        admissao: admissao,
+                        desligamento: desligamento
+                    }
+                }
+            })
+        expect(response.status).to.eq(400);
+        expect(response.body.errors[0]).to.have.property('message', `Variable \"$input\" got invalid value \"\" at \"input.salario_base\"; Float cannot represent non numeric value: \"\"`);
+    })
+
+    it('nao deve criar um funcionario com a admissão vazia', async () => {
+
+        const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .set('Authorization', `Bearer ${token.body.data.login.token}`)
+            .send({
+                query: `mutation CriarFuncionario($input: CriarFuncionarioInput!) {
+                    criarFuncionario(input: $input) {
+                        id
+                        cpf
+                        nome
+                        salario_base
+                        admissao
+                        desligamento
+  }
+}`,
+                variables: {
+                    "input": {
+                        cpf: cpf.toString(),
+                        nome: nome,
+                        salario_base: salario_base,
+                        admissao: "",
+                        desligamento: desligamento
+                    }
+                }
+            })
+        expect(response.status).to.eq(200);
+        expect(response.body.errors[0]).to.have.property('message', `CPF, nome, salário base e admissão são obrigatórios.`);
+    })
+})
---
+++ test/specs/mutations/login.test.js
@@ -0,0 +1,97 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+describe('teste gerais', () => {
+    it('deve realizar login com credenciais validas', async () => {
+       const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: "admin@admin.com",
+                    senha: "123456"
+                }
+            })
+            expect(response.status).to.eq(200);
+            expect(response.body.data.login.token).not.be.empty;
+            expect(response.body.data.login.token).to.be.a.string;
+    })
+
+    it('nao deve realizar login com credenciais invalidas', async () => {
+       const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: "admin@admin.com",
+                    senha: "1234567"
+                }
+            })
+            expect(response.status).to.eq(200);
+            expect(response.body.errors[0]).to.have.property('message', 'Credenciais inválidas ou usuário inativo.');
+    })
+
+    it('nao deve realizar login com senha numerica', async () => {
+       const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: "admin@admin.com",
+                    senha: 123
+                }
+            })
+            expect(response.status).to.eq(400);
+            expect(response.body.errors[0]).to.have.property('message', `Variable \"$senha\" got invalid value 123; String cannot represent a non string value: 123`);
+    })
+
+    it('nao deve realizar login com imail invalido', async () => {
+       const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: "admin@admin",
+                    senha: "1234567"
+                }
+            })
+            expect(response.status).to.eq(200);
+            expect(response.body.errors[0]).to.have.property('message', 'Credenciais inválidas ou usuário inativo.');
+    })
+
+    it('nao deve realizar login com e-mail numerico', async () => {
+       const response = await request('http://localhost:4000/graphql')
+            .post('/graphql')
+            .send({
+                query: `mutation Login($email: String!, $senha: String!) {
+                        login(email: $email, senha: $senha) {
+                            token
+                            }
+                        }`,
+                variables: {
+                    email: 1111,
+                    senha: "1234567"
+                }
+            })
+
+            expect(response.status).to.eq(400);
+            expect(response.body.errors[0]).to.have.property('message', `Variable \"$email\" got invalid value 1111; String cannot represent a non string value: 1111`);
+            expect(response.body.errors[0].extensions).to.have.property('code', `BAD_USER_INPUT`);
+    })
+})
